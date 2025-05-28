9b371f9515c26ec5a63ca2c145a7cd2cfe0d48f3
Javier Vasquez C. <javiandriu21@gmail.com>
2025-05-28 16:35:23 -0500

    Inventory page file fixed

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -3,8 +3,9 @@ class InventoryPage:
         self.page = page
 
     def add_to_cart(self, product_name):
-        locator = self.page.locator(f"text={product_name}").locator("..").locator("button")
-        locator.click()
+        self.page.wait_for_selector(".inventory_item")
+        product_locator = self.page.locator(".inventory_item").filter(has_text=product_name)
+        product_locator.locator("button.btn_inventory").click()
 
     def go_to_cart(self):
         self.page.click("#shopping_cart_container")
